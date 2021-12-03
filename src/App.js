88add7aef7116d4169660c829860b1ee629b71fe
mantiiju <mantii_ju@hotmail.com>
2021-12-03 15:15:32 +0100

    installed google map react and @iconify

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { Icon } from '@iconify/react';
+
 function App() {
   return (
     <div className="App">
       <h1>Wildfire tracker NASA API</h1>
+      <Icon style={{fontSize: '60px', color: 'red'}} icon="mdi-light:home"></Icon>
     </div>
   );
 }
